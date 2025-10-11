2b30f440f2517960aee3135b6ad9aad927397a09
nguyed77 <dung.nguyen@365.elmhurst.edu>
2025-10-11 08:02:06 -0500

    convert time series to mtf

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -10,12 +10,12 @@ df = pd.read_csv('https://raw.githubusercontent.com/nguyend77/pediatric-ecg/refs
 path = 'data/Child_ecg/'
 
 def retrieve_mtf(address):
-    arr = np.zeros((12, 5000, 5000, 1), dtype=np.float64)
+    arr = np.zeros((1, 5000, 5000, 12), dtype=np.float64)
     # crop to first 5000 sampling points (10s)
     ecg = wfdb.rdsamp(path+address)[0][:5000, :]
     for lead in range(12):
         # create a row vector from a 1D array and fit data to MTF
         mtf = MarkovTransitionField(strategy='uniform').fit_transform(ecg[:, lead].reshape(1, -1))
         # output of fit_transform is (1, 5000, 5000), append first element to arr
-        arr[lead, :, :, 0] = mtf[0]
+        arr[0, :, :, lead] = mtf[0]
     return arr

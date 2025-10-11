145451c3dfa35699bf8e81d90712108e0bfdb1d6
nguyed77 <dung.nguyen@365.elmhurst.edu>
2025-10-11 13:56:59 -0500

    complete preprocessing

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -8,7 +8,7 @@ df = pd.read_csv('ecg_data.csv')
 # path to Child_ecg folder
 path = 'data/Child_ecg/'
 # image size for MTF
-size = 512
+size = 256
 
 def retrieve_mtf(address):
     # use float32 to save 50% memmory
@@ -16,15 +16,19 @@ def retrieve_mtf(address):
     # crop to first 4096 sampling points (2^12)
     ecg = wfdb.rdsamp(path+address)[0][:4096, :]
     for lead in range(12):
-        # create a row vector from a 1D array and fit data to MTF, downscale data to 512 points
+        # create a row vector from a 1D array and fit data to MTF, downscale data to 256 points
         mtf = MarkovTransitionField(image_size=size, strategy='uniform').fit_transform(ecg[:, lead].reshape(1, -1))
-        # output of fit_transform is (1, 512, 512), append first element to arr
+        # output of fit_transform is (1, 256, 256), append first element to arr
         arr[:, :, lead] = mtf[0]
     return arr
 
+# save MTFs as X
 filename = df['Filename'].to_list()
 length = len(filename)
-X = np.memmap('X.npy', dtype=np.float32, mode='w+', shape=(length, size, size, 12))
+X = np.memmap('input/X.npy', dtype=np.float32, mode='w+', shape=(length, size, size, 12))
 for i in tqdm(range(length)):
     X[i] = retrieve_mtf(filename[i])
 X.flush()
+
+# save diagnosis as Y
+np.save('input/Y.npy', df['diagnosis'])

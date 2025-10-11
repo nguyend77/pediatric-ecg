6d3f2c517640689d062e0565acb817178d78f614
nguyed77 <dung.nguyen@365.elmhurst.edu>
2025-10-11 00:06:15 -0500

    process data from figshare

---
+++ preprocessing/filter.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('data/AttributesDictionary.csv', usecols=['Filename', 'Sampling_point', 'Lead', 'ICD-10 code'])
+# regular expression (regex) pattern
+cardiovascular_disease = 'I40.0|I40.9|I51.4|I42.0|I42.2|I42.9|Q24.8|M30.3|Q21.0|Q21.1|Q21.2|Q21.3|Q22.1|Q25.0|Q25.6|I37.0'
+df['diagnosis'] = df['ICD-10 code'].str.contains(cardiovascular_disease).astype(int)
+# limit to 12 lead ECGs
+df = df[df['Lead'] == 12]
+# drop records of less than 5000 sampling points (10s)
+df = df[df['Sampling_point'] >= 5000]
+df = df.drop(columns=['Sampling_point', 'Lead', 'ICD-10 code'])
+df.to_csv('ecg_data.csv', index=False)
---
+++ preprocessing/preprocess.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import wfdb
+from pyts.image import MarkovTransitionField
+
+# path to Child_ecg folder
+path = 'data/Child_ecg/'
+
+def retrieve_mtf(address):
+    arr = np.zeros((12, 5000, 5000, 1), dtype=np.float64)
+    # crop to first 5000 sampling points (10s)
+    ecg = wfdb.rdsamp(path+address)[0][:5000, :]
+    for lead in range(12):
+        # create a row vector from a 1D array and fit data to MTF
+        mtf = MarkovTransitionField(strategy='uniform').fit_transform(ecg[:, lead].reshape(1, -1))
+        # output of fit_transform is (1, 5000, 5000), append first element to arr
+        arr[lead, :, :, 0] = mtf[0]
+    return arr
